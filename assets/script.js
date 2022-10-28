dbcfed07180dbe69fe1e5528f360119d48e9d36c
Chris Poturalski <ChrisPoturalski@chriss-mbp.lan>
2022-10-28 14:16:50 -0700

    added criteria to the script.js sheet and then added an assets folder for the CSS styling along with the script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,33 @@
-console.log("Hello world!")
+//Main Goal
+//I want to add events to a daily planner so that I can manage my time effectively
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Criteria
+//When I open the planner, then I see the current day is displayed above the calendar
+//When I scroll down, I see timeblocks for standard business hours
+//When I look at the timeblocks, then each timeblock is color coded to indicate whether it is 
+// in the past, present, or future
+//When I click into a timeblock, then I can enter an event
+//When I click the save button for that time block, then text is saved in local storage
+//When I refresh page, the saved events persits
+
+
+
+
